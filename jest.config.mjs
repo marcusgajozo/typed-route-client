244b505f56ec806e39976e3fcd9fc41f4eeb66e5
Marcus Gajozo <marcusgajozo@gmail.com>
2026-05-27 14:49:40 -0400

    chore(tests): update Jest configuration for TypeScript support and enhance type definitions

--- jest.config.mjs
+++ jest.config.mjs
@@ -19,11 +19,33 @@ const config = {
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   projects: [
     {
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          { useESM: true, tsconfig: 'tsconfig.jest.json' },
+        ],
+      },
+      extensionsToTreatAsEsm: ['.ts', '.tsx'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+      },
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
       displayName: 'unit',
       testEnvironment: 'node',
       testMatch: ['<rootDir>/src/lib/core/**/*.{test,spec}.ts'],
     },
     {
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          { useESM: true, tsconfig: 'tsconfig.jest.json' },
+        ],
+      },
+      extensionsToTreatAsEsm: ['.ts', '.tsx'],
+      moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+      },
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
       displayName: 'react',
       testEnvironment: 'jsdom',
       testMatch: ['<rootDir>/src/lib/react/**/*.{test,spec}.{ts,tsx}'],
