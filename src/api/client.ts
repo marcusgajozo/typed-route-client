1378ce06d81874a110ffabbb10aefad0c6a1dcf2
Marcus Gajozo <marcusgajozo@gmail.com>
2026-05-28 11:20:41 -0400

    refactor(api): replace fetch transport with appHttpTransport and remove obsolete fetch-config

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,11 +1,11 @@
 import { createRouteClient } from '../lib/core/call-route';
 import { createReactQueryHooks } from '../lib/react/create-react-query-hooks';
-import { fetchConfig } from '../libs/fetch-config';
+import { appHttpTransport } from '../libs/http-transport';
 import { apiRoutes } from '../services/api/api-routes';
 
 export const routeClient = createRouteClient({
   routes: apiRoutes,
-  transport: fetchConfig,
+  transport: appHttpTransport,
 });
 
 export const { useApiQuery, useApiMutation } =
