7bafcd2226974a8c68688d7400284184c32054af
Marcus Gajozo <marcusgajozo@gmail.com>
2026-05-29 16:44:51 -0400

    chore: update .gitignore

---
+++ release.config.js
@@ -0,0 +1,19 @@
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'dist/**'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
